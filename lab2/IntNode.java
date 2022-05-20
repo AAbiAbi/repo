2c8d408a2f00435d39e4f8f15a0935a56e8f8b3a
goddashab <ningchenliang98@gmail.com>
2022-05-20 19:57:55 +0800

    complete version of IntNode.java

---
+++ lab2/IntNode.java
@@ -0,0 +1,14 @@
+public class IntNode{
+	public int item;
+	public IntNode next;
+   
+   public IntNode(int i, IntNode n){
+      item = i;
+      next = n;
+      //recursion
+   }
+ 
+
+   /** Return the size of List using Recursion*/
+  
+}
